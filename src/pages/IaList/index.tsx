19d21fcbd8f003fc26794e28d47adf9450b304b4
Q11304 <aq11304@Q11304-2PN1171200047.local>
2024-04-17 15:07:30 +0900

    ialist 추가

---
+++ src/pages/IaList/index.tsx
@@ -0,0 +1,14 @@
+const Index = () => {
+  return (
+    <>
+      <div style={{ width: "1200px", margin: "0 auto", padding: "100px" }}>
+        <h1 style={{ fontSize: 150, fontWeight: 600 }}>React IA List </h1>
+        <button>mini 블로그 보러가기</button>
+        <button>todo List 보러가기</button>
+        <button>tiving 보러가기</button>
+      </div>
+    </>
+  );
+};
+
+export default Index;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,11 +4,13 @@ import Test from "~/pages/test";
 import Main from "~/pages/main";
 import Ids from "~/pages/id";
 import ToDoList from "~/pages/TodoList";
+import IaList from "~/pages/IaList";
 
 const routes = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Main />} />
+      <Route path="/" element={<IaList />} />
+      <Route path="/main" element={<Main />} />
       <Route path="/test" element={<Test />} />
       <Route path="/ids/:id" element={<Ids />} />
       <Route path="/todolist" element={<ToDoList />} />
